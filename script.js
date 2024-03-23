5059bf0fb09e30f24751107ef0160c70e323626d
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-03-23 18:54:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // Convert both strings to lowercase for case-insensitive comparison
+  const lowerCaseS1 = s1.toLowerCase();
+  const lowerCaseS2 = s2.toLowerCase();
+  
+  // Use indexOf to find the index of first occurrence of lowerCaseS2 in lowerCaseS1
+  return lowerCaseS1.indexOf(lowerCaseS2);
 }
 
 // Please do not change the code below
